6d551137ce0ab230c1b5e2e26265b39b8a03e156
yonycherkos <yonycherkos@gmail.com>
2020-09-16 10:30:29 +0300

    use green color for the bounding box

--- build_face_dataset.py
+++ build_face_dataset.py
@@ -50,10 +50,10 @@ while True:
         (startX, startY, endX, endY) = box.astype("int")
 
         # draw the boundidxbox = detections[0, 0, idxng box the frame
-        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
         text = "{:.2f}".format(confidence * 100)
         y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
 
         # display the detected faces
         cv2.imshow("detected face", frame)
