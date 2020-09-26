eb84dacea0f5bd00104d787359ee6ab745f06b22
yonycherkos <yonycherkos@gmail.com>
2020-09-26 23:42:08 +0300

    coloring the table row based its value

--- App/pages/view_attendance.py
+++ App/pages/view_attendance.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append(".")
 
 from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QApplication, QHeaderView
+from PyQt5 import QtGui
 from PyQt5 import uic
 import pandas as pd
 
@@ -17,9 +18,15 @@ class ViewAttendance(QMainWindow):
         self.table.setColumnCount(df.shape[1])
         self.table.setHorizontalHeaderLabels(df.columns)
         self.table.horizontalHeader().setStretchLastSection(True);
+        counts = df.sum(axis=1)/df.shape[1]
         for (i, row) in enumerate(df.values):
+            if counts[i] >= 0.8:
+                color = QtGui.QColor(0, 255, 0, 150)
+            else:
+                color = QtGui.QColor(255, 0, 0, 150)
             for (j, data) in enumerate(row):
                 self.table.setItem(i, j, QTableWidgetItem(str(data)))
+                self.table.item(i, j).setBackground(color)
         header = self.table.horizontalHeader()
         header.setSectionResizeMode(QHeaderView.ResizeToContents)       
         header.setSectionResizeMode(0, QHeaderView.Stretch)
@@ -36,5 +43,4 @@ if __name__ == '__main__':
     window.show()
     app.exec_()
 
-# TODO: color student rows with normal, warning, and danger
 # TODO: add search bar to search and display student info's
