bde51895569c6514ddcb26e18ad8e1f84337f3a3
yonycherkos <yonycherkos@gmail.com>
2020-09-27 00:49:51 +0300

    display detail student info's for a selected row.

--- App/pages/view_attendance.py
+++ App/pages/view_attendance.py
@@ -1,25 +1,26 @@
+import numpy as np
+import pandas as pd
+from PyQt5 import uic
+from PyQt5 import QtGui
+from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QApplication, QHeaderView
 import sys
 sys.path.append(".")
 
-from PyQt5.QtWidgets import QMainWindow, QTableWidgetItem, QApplication, QHeaderView
-from PyQt5 import QtGui
-from PyQt5 import uic
-import pandas as pd
 
 class ViewAttendance(QMainWindow):
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         uic.loadUi("App/ui/viewAttendance.ui", self)
         self.backBtn.clicked.connect(self.back)
-        df = pd.read_csv("output/attendance.csv", index_col=0)
+        self.df = pd.read_csv("output/attendance.csv", index_col=0)
 
-        self.table.setRowCount(df.shape[0])
-        self.table.setColumnCount(df.shape[1])
-        self.table.setHorizontalHeaderLabels(df.columns)
-        self.table.horizontalHeader().setStretchLastSection(True);
-        counts = df.sum(axis=1)/df.shape[1]
-        for (i, row) in enumerate(df.values):
+        self.table.setRowCount(self.df.shape[0])
+        self.table.setColumnCount(self.df.shape[1])
+        self.table.setHorizontalHeaderLabels(self.df.columns)
+        self.table.horizontalHeader().setStretchLastSection(True)
+        counts = self.df.sum(axis=1)/self.df.shape[1]
+        for (i, row) in enumerate(self.df.values):
             if counts[i] >= 0.8:
                 color = QtGui.QColor(0, 255, 0, 150)
             else:
@@ -28,14 +29,26 @@ class ViewAttendance(QMainWindow):
                 self.table.setItem(i, j, QTableWidgetItem(str(data)))
                 self.table.item(i, j).setBackground(color)
         header = self.table.horizontalHeader()
-        header.setSectionResizeMode(QHeaderView.ResizeToContents)       
+        header.setSectionResizeMode(QHeaderView.ResizeToContents)
         header.setSectionResizeMode(0, QHeaderView.Stretch)
+        self.table.clicked.connect(self.detail)
+
+    def detail(self):
+        row = self.table.currentRow()
+        if row > -1:
+            name = self.table.item(row, 0).text()
+            attend_count = self.df.sum(axis=1)[row]
+            num_classes = self.df.shape[1]
+            attend_frac= np.round(attend_count/num_classes, 2)
+            displayText = "name: {}\n absent: {} classes\n attendance: {}%".format(name, (num_classes - attend_count), attend_frac*100)
+            self.detailLabel.setText(displayText)
 
     def back(self):
         from home import HomePage
         self.homePage = HomePage()
         self.homePage.show()
         self.close()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
