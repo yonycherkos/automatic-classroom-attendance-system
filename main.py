a92aedd7204c6a4f3472be84039ef4815ce02167
yonycherkos <yonycherkos@gmail.com>
2020-11-18 20:51:47 +0300

    apply dark theme

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from PyQt5.QtWidgets import QApplication
 from App.pages.login import LoginPage
+import qdarkstyle
 import sys
 
 app = QApplication(sys.argv)
+app.setStyleSheet(qdarkstyle.load_stylesheet(qt_api='pyqt5'))
 window = LoginPage()
 window.show()
 app.exec_()
